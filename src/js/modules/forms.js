f8ff58f8efee06133c72548222cac5d733ee470b
Akhror Sharafutdinov <rgras@mail.ru>
2021-11-20 20:58:12 +0300

    modified WinwowTabs and add WindowCalc script

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,14 +1,9 @@
-const forms = () =>{
+import checkNumInputs from "./checkNumInputs";
+const forms = (state) =>{
     const form = document.querySelectorAll('form'),
-        inputs = document.querySelectorAll('input'),
-        phoneInputs = document.querySelectorAll('input[name = "user_phone"]');
-    
-        phoneInputs.forEach(item =>{
-        item.addEventListener('input', ()=>{
-            item.value = item.value.replace(/\D/,'');
-        });
-    });
-    
+        inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');          
     const message = {
         loading: 'Загрузка...',
         success: 'Спасибо! Скоро мы с Вами свяжимся',
@@ -37,6 +32,11 @@ const forms = () =>{
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
             postData('assets/server.php', formData)
             .then(res =>{
                 console.log(res);
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,23 +1,34 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]');
         trigger.forEach(item =>{
             item.addEventListener('click', (e) => {
                 if (e.target){
                     e.preventDefault();
                 }
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
             });
         });
         close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
             modal.style.display = 'none';
             document.body.style.overflow = '';
         });
         modal.addEventListener('click', (e) =>{
-            if (e.target === modal){
+            if (e.target === modal && closeClickOverlay){
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
             }
@@ -31,6 +42,9 @@ const modals = () => {
     }
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     /* showModalByTime('.popup', 60000); */
 };
 
