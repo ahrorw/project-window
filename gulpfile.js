d9109f8ea71c14529c42d2be0f75b0f4672ecb80
Sharafutdinov Akhror <u@example.com>
2021-11-24 17:01:39 +0300

    add Deadline-timer modules

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-/* const dist = "./dist/"; */
-const dist = "../../OpenServer/domains/dist";
+const dist = "./dist/";
+/* const dist = "../../OpenServer/domains/dist"; */
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
