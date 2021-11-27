6c8382919c47ffc83ac23e9ca036b4c64805df6e
Akhror Sharafutdinov <rgras@mail.ru>
2021-11-27 11:17:19 +0300

    add something animation

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
         trigger.forEach(item =>{
             item.addEventListener('click', (e) => {
                 if (e.target){
@@ -15,6 +16,7 @@ const modals = () => {
 
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
             });
         });
         close.addEventListener('click', () => {
@@ -23,6 +25,7 @@ const modals = () => {
             });
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
         });
         modal.addEventListener('click', (e) =>{
             if (e.target === modal && closeClickOverlay){
@@ -31,6 +34,7 @@ const modals = () => {
                 });
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
             }
         });       
     }
@@ -39,6 +43,17 @@ const modals = () => {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = 'hidden';
         }, time);
+    }
+    function calcScroll(){
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
     }
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
